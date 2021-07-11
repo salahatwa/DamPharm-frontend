ea88b1d6f5645aab212db17be9151a90a37621e4
salahatwa <salahsayedatwa@gmail.com>
2021-07-11 13:54:29 +0200

    Adding heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ const { name, description, version } = require('../../package.json');
 
 export const environment = {
   production: true,
-  api: 'http://localhost:8080/api',
-  unauth_api:'http://localhost:8080/unauth',
+  api: 'https://dampharm-backend.herokuapp.com/api',
+  unauth_api: 'http://localhost:8080/unauth',
   name,
   description,
   version
