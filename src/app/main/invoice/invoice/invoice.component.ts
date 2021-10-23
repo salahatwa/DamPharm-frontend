76eb542ff7e9035c716abdc5635b8b3e3cbb258a
salahatwa <salahsayedatwa@gmail.com>
2021-10-23 22:35:28 +0200

    adding bonus

--- src/app/main/invoice/invoice/invoice.component.ts
+++ src/app/main/invoice/invoice/invoice.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ReportViewerDialogComponent } from 'src/app/shared/components/report-dialogs/report-viewer-dialog/report-viewer.component';
+import { finalize } from 'rxjs/operators';
 import { Customer } from './../../../core/classes/customer';
 import { IInvoice } from './../../../core/classes/invoice';
 import { Product } from './../../../core/classes/product';
 import { CustomerService } from './../../../core/services/customer.service';
 import { InvoiceService } from './../../../core/services/invoice.service';
 import { ProductService } from './../../../core/services/product.service';
-import { finalize } from 'rxjs/operators';
 declare var Stimulsoft: any;
 
 @Component({
@@ -19,18 +16,14 @@ declare var Stimulsoft: any;
 })
 export class InvoiceComponent implements OnInit {
   form: FormGroup;
-  phoneCustomer: "";
-  addressCustomer: "";
+  selectedCustomer: Customer;
   showDiv: true;
   customer: true;
-  private selectUndefinedOptionValue: any;
   customerList: Customer[];
   productList: Product[];
-  showDialog1;
-  showDialog;
   loading: boolean = false;
 
-  constructor(private router: Router, private _fb: FormBuilder, private invoiceService: InvoiceService, private customerService: CustomerService, private productService: ProductService) { }
+  constructor(private _fb: FormBuilder, private invoiceService: InvoiceService, private customerService: CustomerService, private productService: ProductService) { }
 
   ngOnInit() {
     this.loading = true;
@@ -43,7 +36,6 @@ export class InvoiceComponent implements OnInit {
       this.loading = false;
     })).subscribe(data => {
       this.productList = data;
-      console.log(data);
     });
     this.initForm();
   }
@@ -73,13 +65,6 @@ export class InvoiceComponent implements OnInit {
     for (let i in items) {
       let amount = (items[i].quantity * items[i].product.price);
 
-      // console.log(items[i].product.availableQuantity + "-" + items[i].quantity);
-      // const old = items[i].product.availableQuantity;
-      // const availableQuantity =  old - items[i].quantity;
-      // items[i].product.availableQuantity = availableQuantity;
-      // control.at(+i).get('product').setValue(items[i].product, { onlySelf: true, emitEvent: false });
-      // this.updateProductList(items[i].product);
-
       let discoubtControlValue = control.at(+i).get('discount').value;
 
 
@@ -98,15 +83,15 @@ export class InvoiceComponent implements OnInit {
     const numberPatern = '^[0-9.,]+$';
     return this._fb.group({
       product: [product, Validators.required],
-      quantity: [1, [Validators.required, Validators.pattern(numberPatern)]],
-      discount: [0],
+      quantity: [1, [Validators.max(product.availableQuantity), Validators.required, Validators.pattern(numberPatern)]],
+      discount: [0, [Validators.max(product.price), Validators.pattern(/^[0-9]+(\.[0-9]{1,2})?$/)]],
+      bonus: [0, [Validators.max(product.availableQuantity), Validators.pattern(numberPatern)]],
       amount: [{ value: 0, disabled: true }],
     });
   }
 
   money(value: number) {
-    let tmoney = this.invoiceService.money(value);
-    return tmoney;
+    return this.invoiceService.money(value);
   }
 
   public addPurchase(product: Product): void {
@@ -114,20 +99,17 @@ export class InvoiceComponent implements OnInit {
     let add = true;
     for (let i in control.controls) {
       if (control.at(+i).get('product').value.id === product.id) {
-        // control.controls[i].get('quantity').setValue(control.controls[i].controls.quantity.value + 1);
         control.at(+i).get('quantity').setValue(control.at(+i).get('quantity').value + 1);
-
         add = false;
       }
     }
     if (add) {
-      console.log(product);
       control.push(this.purchaseForm(product));
       this.showDiv = add;
     }
   }
 
-  private removePurchase(i: number): void {
+  removePurchase(i: number): void {
     const control = <FormArray>this.form.controls['items'];
     control.removeAt(i);
   }
@@ -140,7 +122,7 @@ export class InvoiceComponent implements OnInit {
 
   updateAvailableQuantity(invoice: IInvoice) {
     invoice.items.forEach(item => {
-      item.product.availableQuantity = item.product.availableQuantity - item.quantity;
+      item.product.availableQuantity = item.product.availableQuantity - item.quantity - item.bonus;
     });
   }
 
@@ -152,14 +134,12 @@ export class InvoiceComponent implements OnInit {
       // Do useful stuff with the gathered data
 
       this.updateAvailableQuantity(result);
-      console.log(result);
 
       this.invoiceService.insertInvoice(result).pipe(finalize(() => {
         this.loading = false;
       })).subscribe((data => {
         this.invoiceService.openInvoiceDialog(data);
-        this.phoneCustomer = '';
-        this.addressCustomer = '';
+        this.selectedCustomer=null;
         this.showDiv = null;
         //Agregar msg
         this.initForm();
@@ -184,23 +164,7 @@ export class InvoiceComponent implements OnInit {
   getSelectedOptionText(event) {
     this.resetPurchase();
     this.showDiv = null;
-    this.phoneCustomer = event.phone;
-    this.addressCustomer = event.address;
+    this.selectedCustomer=event;
   }
 
-
-  print() {
-
-    // this.invoiceService.insertInvoice(result).subscribe((data => {
-    //   this.invoiceService.openInvoiceDialog(data);
-    //   this.phoneCustomer = '';
-    //   this.addressCustomer = '';
-    //   this.showDiv = null;
-    //   //Agregar msg
-    //   this.initForm();
-    //   // this.router.navigate(['/invoices-list']);
-    // }));
-
-
-  }
 }
